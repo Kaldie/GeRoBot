b7ca189e52d60fba02e1c8a34e85e895b93719e1
Kaldie <ruudcools@gmail.com>
2014-11-03 14:32:09 +0100

    removed unnesesary includes

--- motor/Trace.h
+++ motor/Trace.h
@@ -1,29 +1,28 @@
 #ifndef Trace_h
 #define Trace_h
 
-#include <string>
 #include <macroHeader.h>
-#include <Point2D.h>
-#include <Line2D.h>
+
+class Point2D;
+class Line2D;
 
 enum TRACE_TYPE{Line,
-		Curve};
+								Curve};
 
 class Trace
 {
  private:
 
   GETSET(TRACE_TYPE,m_traceType,TraceType); 
-  GETSET(Point2D,m_startPosition,StartPosition);
-  GETSET(Point2D,m_endPosition,EndPosition);
+  GETSET(Point2D,m_startPoint,StartPoint);
+  GETSET(Point2D,m_endPoint,EndPoint);
 
   GETSET(float,m_rotationTolerance,RotationTolerance);
   GETSET(float,m_translationTolerance,TranslationTolerance);
 
-
-  bool isValidStartAndEndPosition(const Point2D*,
-				  const Point2D*);
-
+	bool isValidStartAndEndPoint(const Point2D*,
+															 const Point2D*);
+	
 	bool isWithinRange(const Point2D&, const Point2D&) const;
   bool isWithinRange(const float&, const float&) const;
   
@@ -33,7 +32,7 @@ class Trace
   Trace();
   Trace(const Point2D&,  //Start Point
 				const Point2D&); //End point
-
+	
   Trace(const Point2D&,  //Start point
 				const Point2D&,  //End point
 				const TRACE_TYPE&);
@@ -43,10 +42,12 @@ class Trace
   
   bool isWithinEndRange(const Point2D&) const;
 	bool isWithinBeginRange(const Point2D&) const;
+
+	virtual Point2D intersectingPoint(const Point2D& i_currentPosition)const;
   
   std::string getRotationDirectionToEndPoint(const Point2D&) const;
 	std::string getRotationDirection(const Point2D&, const Point2D&) const;
-
+	
 	std::string getTranslationDirectionToEndPoint(const Point2D&) const;
 	std::string getTranslationDirection(const Point2D&, const Point2D&) const;
 };
