d5128c338412a68aff8393058dd550731adb9cac
kaldie <ruudcools@gmail.com>
2015-04-01 22:33:30 +0200

    Use bytebuffer to store the messages, use interupt to set the pins and use the main loop to request messages from the host. Future: use SD card to store the messages and retrieve them from there, so we can actualy get the speed we need

--- arduinosketch/ArduinoStepper.cpp
+++ arduinosketch/ArduinoStepper.cpp
@@ -3,35 +3,68 @@
 #include <string.h>
 #include <stdarg.h>
 #include <Arduino.h>
-
+#include <ByteBuffer.h>
 // defines
 #define BAUD_RATE 115200
 #define HAND_SHAKE 200
 
-// Global variables definitions
-bool HAS_SHAKEN = false;
+// States for the machine
+enum Status {
+  SEND_HAND_SHAKE,
+  VERIFY_HAND_SHAKE,
+  HANDLE_INT_SIZE,
+  RECEIVING_MESSAGE_SIZE,
+  RECEIVING_SPEED,
+  RECEIVING_REPITITIONS,
+  RECEIVING_STEPS,
+  SEND_CRC,
+  VERIFY_CRC,
+  WRITE_IN_BUFFER,
+};
+
+
+// data structure which represent the motor message
+struct MotorMessage {
+  int numberOfSteps;
+  int currentStep;
+  int speed;
+  int numberOfRepetitions;
+  int stepArray[30];
+  unsigned char currentCRC;
+};
+
+
+// State which represent the return of the handle functions
+enum ReturnState {
+  NO_RESPONSE,
+  FAIL,
+  SUCCES
+};
+
+// Global variables
+// Size of an received in bytes
 int SIZE_OF_INT = 0;
-unsigned long DELAY_BEFORE_READ = 0.1;
-// Global buffer to store integer values received from serial connection
-byte INTEGER_BUFFER[10];
 
-// Global step variables
-int STEP_ARRAY[100];
+// Buffer needed to write the integers from the serial connection
+unsigned char INTEGER_BUFFER[10];
 
-// number of repetitions which need to be done
-int NUMBER_OF_REPETITIONS = 0;
-// number of steps which are defined in the step array
-int NUMBER_OF_STEPS = 0;
-int CURRENT_STEP = 0;
+// buffer for motor messages
+ByteBuffer<MotorMessage> MOTOR_MESSAGE_BUFFER(20);
 
-// set up functions
-extern void setPins(byte i_pinValue);
-extern bool handShake();
-extern void sendSOSLed();
-extern int readIntegerFromSerial(bool&);
+// return state of the handle functions
+ReturnState RETURN_STATE;
 
-// example buffer filling
-extern void blinkKnightRider();
+// number of failed attempts to handle a certain state
+int ATTEMPTS = 0;
+
+// status of the state machine
+Status status = SEND_HAND_SHAKE;
+
+// Is the motor currently running?
+bool IS_MOTOR_RUNNING = false;
+
+void sendSOSLed();
+void getMessageFromBuffer();
 
 
 void disableHeartBeat() {
@@ -50,7 +83,6 @@ void setTimer1Interupt(int i_dekaHertz) {
   // i_dekaHertz is 10* input hertz
   // so we can send 100k hertz with arduino 2 byte int
   cli();  // stop interrupts
-  digitalWrite(13, HIGH);
 
   // set timer1 interrupt at 1Hz
   TCCR1A = 0;  // set entire TCCR1A  to 0
@@ -95,126 +127,109 @@ void setPinOutputs() {
 
 
 void setPins(byte i_byte) {
-    // no1 touches the 1st and 2nd pin;
-    i_byte &= B11111100;
-    PORTD = i_byte;
+  // no1 touches the 1st and 2nd pin;
+  i_byte &= B11111100;
+  PORTD = i_byte;
 }
 
 
 ISR(TIMER1_COMPA_vect) {
-  setPins((byte)(STEP_ARRAY[CURRENT_STEP]));
+  MotorMessage* message = MOTOR_MESSAGE_BUFFER.getReadPointer();
+  setPins((byte)(
+      message->stepArray[message->currentStep]));
   /*
     Serial.write(STEP_ARRAY[CURRENT_STEP] + '0');
     Serial.write(CURRENT_STEP + '0');
     Serial.write(NUMBER_OF_REPETITIONS + '0');
     Serial.write(NUMBER_OF_STEPS + '0');
   */
-  CURRENT_STEP++;
+  message->currentStep++;
 
-  if (CURRENT_STEP >= NUMBER_OF_STEPS) {
-    NUMBER_OF_REPETITIONS--;
-    CURRENT_STEP = 0;
+  if (message->currentStep >=
+      message->numberOfSteps) {
+    message->numberOfRepetitions--;
+    message->currentStep = 0;
   }
 
-  if (NUMBER_OF_REPETITIONS <= 0) {
+  if (message->numberOfRepetitions <= 0) {
     disableHeartBeat();
-    HAS_SHAKEN = false;
+    IS_MOTOR_RUNNING = false;
   }
 }
 
 
-bool readIntegerFromSerial(int& i_value) {
-  int attempts = 0;
-  while (Serial.available() < SIZE_OF_INT & attempts < 1000) {
-    attempts++;
-    delay(DELAY_BEFORE_READ);
+void readIntegerFromSerial(int& i_value,
+                           ReturnState* i_returnState) {
+  if (Serial.available() < SIZE_OF_INT) {
+    *i_returnState = NO_RESPONSE;
+    return;
+  } else {
+    *i_returnState = SUCCES;
   }
-
-  if (Serial.available() < SIZE_OF_INT)
-    return false;
-
-  for (int i = 0 ;
+  
+  for (int i = 0;
        i < SIZE_OF_INT;
        i++)
     INTEGER_BUFFER[i] = Serial.read();
   i_value = *(reinterpret_cast<int*>(INTEGER_BUFFER));
-  return true;
 }
 
 
 void blinkKnightRider() {
-  NUMBER_OF_STEPS = 12;
-  NUMBER_OF_REPETITIONS = 5;
-  CURRENT_STEP = 0;
-  for (int i = 2;
-       i < 8;
-       i++) {
-    STEP_ARRAY[i-2] = 1 << i;
-  }
+  if (!MOTOR_MESSAGE_BUFFER.isFull()) {
+    MotorMessage* message = MOTOR_MESSAGE_BUFFER.getWritePointer();
+    message->numberOfSteps = 12;
+    message->numberOfRepetitions = 2;
+    message->currentStep = 0;
+    message->speed = 1;
+    for (int i = 2;
+         i < 8;
+         i++) {
+      message->stepArray[i-2] = 1 << i;
+    }
 
-  for (int i = 7;
-       i > 1;
-       i--) {
-    STEP_ARRAY[6-(i-7)] = 1 << i;
-  }
+    for (int i = 7;
+         i > 1;
+         i--) {
+      message->stepArray[6-(i-7)] = 1 << i;
+    }
 
-  setTimer1Interupt(5);
+
+    MOTOR_MESSAGE_BUFFER.finishWritePointer();
+    IS_MOTOR_RUNNING = true;
+    getMessageFromBuffer();
+  } else {
+    MOTOR_MESSAGE_BUFFER.finishReadPointer();
+  }
 }
 
 
-bool handShake() {
-  // Hand shake with the controller
-  int intSize = 0;
-  bool received = false;
-  bool hasIntSize = false;
-  byte buffer[1];
-  int attempts = 0;
-
+bool sendHandShake(ReturnState* i_returnState) {
   Serial.write(HAND_SHAKE);
-  while (!received & attempts < 1000) {
-    if (Serial.available() > 0) {
-      // receive echo
-      Serial.readBytes((char*)buffer, 1);
-      if (*buffer == HAND_SHAKE) {
-        received = true;
-      };
-    } else {
-      attempts++;
-      delay(DELAY_BEFORE_READ);
-    }
+  *i_returnState = SUCCES;
+}
+
+
+void handleIntSize(ReturnState* i_returnState) {
+  if (Serial.available() < 1) {
+    *i_returnState = NO_RESPONSE;
+    return;
   }
 
-  if (!received) {
-    return false;
+  // read the int size of the controller
+  byte buffer[1];
+  int intSize = 0;
+  Serial.readBytes((char*)buffer, 1);
+  intSize = *buffer;
+
+  if (intSize < 10) {
+    // echo it if its ok
+    Serial.write(intSize);
+    *i_returnState = SUCCES;
+    SIZE_OF_INT = intSize;
+  } else {
+    *i_returnState = FAIL;
   }
-
-  attempts = 0;
-  while( !hasIntSize & attempts < 100) {
-    if (Serial.available() > 0) {
-      // read the int size of the controller
-      Serial.readBytes((char*)buffer, 1);
-      intSize = *buffer;
-
-      if (intSize < 10) {
-        // echo it if its ok
-        Serial.write(intSize);
-      }
-
-      SIZE_OF_INT = intSize;
-      hasIntSize = true;
-    } else {
-      attempts++;
-    }
-  }
-
-  if (!hasIntSize) {
-    Serial.write(3);
-  }
-
-  if (hasIntSize & received)
-    return true;
-  else
-    return false;
 }
 
 
@@ -227,78 +242,167 @@ void sendSOSLed() {
 }
 
 
-bool verifyCRC(unsigned char i_crcNumber) {
-  unsigned char receivedCRC;
-  int attempts = 0;
-  Serial.write(i_crcNumber);
-
-  do {
-    Serial.readBytes(reinterpret_cast<char*>(&receivedCRC), 1);
-    delay(DELAY_BEFORE_READ);
-    attempts++;
-  } while (receivedCRC == -1 & attempts < 100);
-
-  if (receivedCRC != i_crcNumber) {
-    NUMBER_OF_REPETITIONS = 0;
-    HAS_SHAKEN = false;
-    return false;
+void verifyResponse(const unsigned char& i_requiredResponse,
+                    ReturnState* i_returnState) {
+  unsigned char input;
+  if (Serial.available() == 0) {
+    *i_returnState = NO_RESPONSE;
+    return;
   } else {
-    return true;
+    Serial.readBytes(reinterpret_cast<char*>(&input),
+                     1);
+  }
+
+  if (input == i_requiredResponse) {
+    *i_returnState = SUCCES;
+  } else {
+    // input is not equal to required response
+    *i_returnState = FAIL;
   }
 }
 
 
-bool handleMotorMessage() {
-  // add all values received from serial
-  // to each other and send them back as a check
-  unsigned char crcNumber = 0;
-  unsigned char receivedCRC = 0;
-
-  // number of bytes in the current message
-  int numberOfBytesInMessage;
-  // speed of the state changes
-  int speed;
-  
-  if (!readIntegerFromSerial(numberOfBytesInMessage)) {
-    verifyCRC(crcNumber);
-    return false;
-  }
-  
-  // number of steps per repetitions;
-  NUMBER_OF_STEPS = (numberOfBytesInMessage / SIZE_OF_INT) - 2;
- 
-  CURRENT_STEP = 0;
-  
-  if (!readIntegerFromSerial(speed)) {
-    verifyCRC(crcNumber);
-    return false;
-  }
-
-  if (!readIntegerFromSerial(NUMBER_OF_REPETITIONS)) {
-    verifyCRC(crcNumber);
-    return false;
-  }
-
-  // calculate the crc number
-  crcNumber += numberOfBytesInMessage;
-  crcNumber += speed;
-  crcNumber += NUMBER_OF_REPETITIONS;
-
-  for (int i = 0;
-       i < NUMBER_OF_STEPS;
-       i++) {
-    if (readIntegerFromSerial(STEP_ARRAY[i])) {
-      crcNumber += STEP_ARRAY[i];
-    } else {
-      verifyCRC(crcNumber);
-      return false;
+void handleStatus() {
+  switch (status) {
+    case SEND_HAND_SHAKE: {
+      sendHandShake(&RETURN_STATE);
+      status = VERIFY_HAND_SHAKE;
+      break;
     }
-  }
-  if (verifyCRC(crcNumber)) {
-    setTimer1Interupt(speed);
-    return true;
-  } else {
-    return false;
+
+    case VERIFY_HAND_SHAKE: {
+      verifyResponse(HAND_SHAKE, &RETURN_STATE);
+      if (RETURN_STATE == SUCCES) {
+        status = HANDLE_INT_SIZE;
+        ATTEMPTS = 0;
+      } else if (RETURN_STATE == FAIL) {
+        status = SEND_HAND_SHAKE;
+      }
+      
+      if (ATTEMPTS >= 10000) {
+        ATTEMPTS = 0;
+        status = SEND_HAND_SHAKE;
+      }
+      ATTEMPTS++;
+      break;
+    }
+
+    case HANDLE_INT_SIZE: {
+      handleIntSize(&RETURN_STATE);
+      if (RETURN_STATE == SUCCES) {
+        status = RECEIVING_MESSAGE_SIZE;
+      } else if (RETURN_STATE == FAIL) {
+        status = SEND_HAND_SHAKE;
+      }
+
+      break;
+    }
+
+    case RECEIVING_MESSAGE_SIZE: {
+      int messageSize;
+      readIntegerFromSerial(messageSize, &RETURN_STATE);
+      if (RETURN_STATE == SUCCES) {
+        MOTOR_MESSAGE_BUFFER.getWritePointer()->numberOfSteps =
+            (messageSize / SIZE_OF_INT) - 2;
+        MOTOR_MESSAGE_BUFFER.getWritePointer()->currentCRC = messageSize;
+        status = RECEIVING_SPEED;
+      }
+      break;
+    }
+
+    case RECEIVING_SPEED: {
+      readIntegerFromSerial(MOTOR_MESSAGE_BUFFER.getWritePointer()->speed,
+                            &RETURN_STATE);
+      if (RETURN_STATE == SUCCES) {
+        MOTOR_MESSAGE_BUFFER.getWritePointer()
+            ->currentCRC += MOTOR_MESSAGE_BUFFER.getWritePointer()->speed;
+
+        status = RECEIVING_REPITITIONS;
+      }
+      break;
+    }
+
+    case RECEIVING_REPITITIONS: {
+      readIntegerFromSerial(
+          MOTOR_MESSAGE_BUFFER.getWritePointer()->numberOfRepetitions,
+          &RETURN_STATE);
+      if (RETURN_STATE == SUCCES) {
+        MOTOR_MESSAGE_BUFFER.getWritePointer()->currentCRC +=
+            MOTOR_MESSAGE_BUFFER.getWritePointer()->numberOfRepetitions;
+
+        status = RECEIVING_STEPS;
+        MOTOR_MESSAGE_BUFFER.getWritePointer()->currentStep = 0;
+      }
+      break;
+    }
+
+    case RECEIVING_STEPS: {
+      int step;
+      readIntegerFromSerial(step, &RETURN_STATE);
+      if (RETURN_STATE == SUCCES) {
+        MOTOR_MESSAGE_BUFFER.getWritePointer()->stepArray[
+            MOTOR_MESSAGE_BUFFER.getWritePointer()->currentStep] = step;
+
+        MOTOR_MESSAGE_BUFFER.getWritePointer()->currentCRC +=
+            MOTOR_MESSAGE_BUFFER.getWritePointer()->stepArray[
+                MOTOR_MESSAGE_BUFFER.getWritePointer()->currentStep];
+
+        MOTOR_MESSAGE_BUFFER.getWritePointer()->currentStep++;
+      }
+
+      if (MOTOR_MESSAGE_BUFFER.getWritePointer()->currentStep >=
+          MOTOR_MESSAGE_BUFFER.getWritePointer()->numberOfSteps) {
+        status = SEND_CRC;
+        MOTOR_MESSAGE_BUFFER.getWritePointer()->currentStep = 0;
+      }
+      break;
+    }
+
+    case SEND_CRC: {
+      Serial.write(MOTOR_MESSAGE_BUFFER.getWritePointer()->currentCRC);
+      status = VERIFY_CRC;
+      RETURN_STATE = SUCCES;
+      break;
+    }
+
+    case VERIFY_CRC: {
+      verifyResponse(MOTOR_MESSAGE_BUFFER.getWritePointer()->currentCRC,
+                     &RETURN_STATE);
+      if (RETURN_STATE == SUCCES) {
+        status = WRITE_IN_BUFFER;
+      } else if (RETURN_STATE == FAIL) {
+        status = SEND_HAND_SHAKE;
+      }
+      //      MOTOR_MESSAGE_BUFFER.getWritePointer()->currentCRC = 0;
+      break;
+    }
+
+    case WRITE_IN_BUFFER: {
+      if (!MOTOR_MESSAGE_BUFFER.isFull()) {
+        MOTOR_MESSAGE_BUFFER.finishWritePointer();
+        RETURN_STATE = SUCCES;
+        status = SEND_HAND_SHAKE;
+      } else {
+        //        MOTOR_MESSAGE_BUFFER.getReadPointer();
+        //        MOTOR_MESSAGE_BUFFER.finishReadPointer();
+        RETURN_STATE = FAIL;
+      }
+      break;
+    }
+
+    default: {
+      // sendSOSLed();
+    }
+  }  // end switch
+}
+
+
+void getMessageFromBuffer() {
+  if (!MOTOR_MESSAGE_BUFFER.isEmpty()) {
+    // go to the next message
+    MOTOR_MESSAGE_BUFFER.finishReadPointer();
+    setTimer1Interupt(MOTOR_MESSAGE_BUFFER.getReadPointer()->speed);
+    IS_MOTOR_RUNNING = true;
   }
 }
 
@@ -313,12 +417,13 @@ void setup() {
 
 
 void loop() {
-  // start with a hand shake if that didnt happen yet
-  while (!HAS_SHAKEN){
-    HAS_SHAKEN = handShake();
+  // Make it run!
+  if (!IS_MOTOR_RUNNING) {
+    //    blinkKnightRider();
+    getMessageFromBuffer();
   }
 
-  if (HAS_SHAKEN & NUMBER_OF_REPETITIONS <= 0)
-    handleMotorMessage();
-  delay(5);
+  // start with a hand shake if that didnt happen yet
+  handleStatus();
 }
+
