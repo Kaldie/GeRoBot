2cc1d6030b41de606e8b63f09276016552c3b708
kaldie <ruudcools@gmail.com>
2015-04-01 22:34:48 +0200

    Bytebuffer is overhoaled, using pointer now to point to the items in the buffer, Use finish read/write to go to the next pointer. A NULL pointer is returned if there is no room or no items

--- arduino/ByteBuffer.h
+++ arduino/ByteBuffer.h
@@ -14,156 +14,114 @@
 */
 
 template <typename T>
-class ByteBuffer
-{
+class ByteBuffer {
  private:
+  static const int DEFAULT_SIZE = 10;
+  T* m_readPosition;
+  T* m_writePosition;
+  T* m_buffer;
+  T* m_endOfBuffer;
 
-    static const  int DEFAULT_SIZE=10;
-     int m_readPosition;
-     int m_writePosition;
-     int m_availableStorage;
-     int m_size;
-    T* m_buffer;
-
-    void incrementPosition( int&);
+  void incrementPosition(T*& i_position);
 
 
  public:
+  // Constructors
+  ByteBuffer();
+  explicit ByteBuffer(const int& i_size);
 
-    //Constructors
-    ByteBuffer();
-    ByteBuffer(const int& i_size);
-    
-    //Deconstructor
-    ~ByteBuffer();
+  // Deconstructor
+  ~ByteBuffer();
 
-    //Actual methods
-    void setSize(const int&);
+  T* getReadPointer();
+  bool finishReadPointer();
 
-    const T get();
-    void set(const T& i_value);
-    
-    const  int getAvailableElements();
-    const  int getAvailableStorage();
-    
+  T* getWritePointer();
+  bool finishWritePointer();
+
+  const bool isFull();
+  const bool isEmpty();
 };
 
-//Constructors
+// Constructors
 template <typename T>
-ByteBuffer<T>::ByteBuffer():m_readPosition(0),m_writePosition(0),m_availableStorage(DEFAULT_SIZE),m_size(DEFAULT_SIZE)
-{
-    m_buffer = (T*)malloc(sizeof(T)*m_size);
+ByteBuffer<T>::ByteBuffer()
+: ByteBuffer(DEFAULT_SIZE)
+{}
+
+
+template <typename T>
+ByteBuffer<T>::ByteBuffer(const int& i_size) {
+  m_buffer = (T*)malloc(sizeof(T) * i_size);
+  memset(m_buffer, 0, sizeof(T) * i_size);
+  m_endOfBuffer = m_buffer + i_size;
+  m_readPosition = m_buffer;
+  m_writePosition = m_buffer + 1;
 }
 
-    
+// Deconstructor
 template <typename T>
-ByteBuffer<T>::ByteBuffer(const int& i_size):m_readPosition(0),m_writePosition(0),m_availableStorage(i_size),m_size(i_size)
-{
-    m_buffer = (T*)malloc(sizeof(T)*m_size);
-}
-
-//Deconstructor
-template <typename T>
-ByteBuffer<T>::~ByteBuffer()
-{
+ByteBuffer<T>::~ByteBuffer() {
     free(m_buffer);
 }
 
 template <typename T>
-void ByteBuffer<T>::incrementPosition( int& i_position)
-{
-    if(i_position+1==m_size)
-	i_position=0;
-    else
-	i_position++;
+void ByteBuffer<T>::incrementPosition(T*& i_position) {
+  i_position++;
+  if (m_endOfBuffer == i_position)
+    i_position = m_buffer;
+}
+
+
+// Actual methods
+template <typename T>
+const bool ByteBuffer<T>::isFull() {
+  T* testPointer =  m_writePosition;
+  incrementPosition(testPointer);
+  return testPointer == m_readPosition;
 }
 
 
 template <typename T>
-void ByteBuffer<T>::setSize(const int& i_size)
-{
-    m_size=i_size;
-    m_availableStorage=i_size;
-    free(m_buffer);
-    m_buffer = (T*)malloc(sizeof(T)*m_size);
+const bool ByteBuffer<T>::isEmpty() {
+  T* testPointer =  m_readPosition;
+  incrementPosition(testPointer);
+  return testPointer == m_writePosition;
 }
 
 
-//Actual methods
-template <typename T>
-const  int ByteBuffer<T>::getAvailableStorage()
-{
-    return m_availableStorage;
+template<typename T>
+T* ByteBuffer<T>::getReadPointer() {
+  return m_readPosition;
 }
 
 
-template <typename T>
-const int ByteBuffer<T>::getAvailableElements()
-{
-    //    Serial.print("Size and storage: ");
-    /*    Serial.print(m_size);
-    Serial.print(", ");
-    Serial.print(m_availableStorage);
-    Serial.print(", ");
-    Serial.println(m_size-m_availableStorage);
-    */
-    return (m_size-m_availableStorage);
+template<typename T>
+bool ByteBuffer<T>::finishReadPointer() {
+  if (isEmpty()) {
+    return false;
+  } else {
+    incrementPosition(m_readPosition);
+    return true;
+  }
 }
 
 
-template <typename T>
-const T ByteBuffer<T>::get()
-{
-    //    int availableElements=getAvailableElements();
-    if (getAvailableElements() > 0 )
-	{
-	    //	    Serial.print("Reading position: ");
-	    //	    Serial.println(m_readPosition);
-	    //	    Serial.print(" which is filled with: ");
-	    
-	    T output=(*(m_buffer+m_readPosition));
-	    incrementPosition(m_readPosition);
-	    m_availableStorage++;
-	    //	    Serial.write(output);
-
-	    //   Serial.print(m_readPosition);
-	    //Serial.print(" ");
-	    //	    Serial.println(m_writePosition);
-	    //	    Serial.print("Available Elements: ");
-	    //	    Serial.println(getAvailableElements());
-	    return output;
-	}
-    else
-	{
-	    //	    Serial.print("Available Elements: ");
-	    //	    Serial.println(getAvailableElements());
-	    return T();
-	}
+template<typename T>
+T* ByteBuffer<T>::getWritePointer() {
+  return m_writePosition;
 }
 
 
-template <typename T>
-void ByteBuffer<T>::set(const T& i_value)
-{
-    if(getAvailableStorage()>0)
-	{
-	    /*
-	      Serial.print("Writing: ");
-	      Serial.print(i_value);
-	      Serial.print(".  Writing @: ");
-	      Serial.println(m_writePosition);
-	    */
-	    m_buffer[m_writePosition]=i_value;
-	    //Serial.print("now in buffer: ");
-	    //	    Serial.println(* (m_buffer+m_writePosition));
-	    incrementPosition(m_writePosition);
-	    m_availableStorage--;
-	}
-
-    //    Serial.print("AvailableStorage: ");
-    //    Serial.println(getAvailableStorage());
-    
+template<typename T>
+bool ByteBuffer<T>::finishWritePointer() {
+  if (isFull()) {
+    return false;
+  } else {
+    incrementPosition(m_writePosition);
+    return true;
+  }
 }
 
-#endif // guard header
+#endif  // ARDUINOSKETCH_BYTEBUFFER_H_
 
