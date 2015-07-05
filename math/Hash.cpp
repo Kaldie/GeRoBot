3676bbd548376c66262e4c7409fae2dcd7607221
kaldie <ruudcools@gmail.com>
2015-07-05 22:43:30 +0200

    Start of the unit tests in the math folder

--- math/Hash.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright [2014] Ruud Cools
-
-#include <macroHeader.h>
-
-std::size_t HashUtility::hasRange(vector<int>::const_iterator i_begin,
-                                  vector<int>::const_iterator i_end)
-
-
-
