ae47066709bb3901ae9e315392328b8a4fc06117
kaldie <ruudcools@gmail.com>
2015-08-26 22:30:25 +0200

    Small modifications to vector to make life easier

--- math/Vector2D.cpp
+++ math/Vector2D.cpp
@@ -17,20 +17,23 @@ Vector2D& Vector2D::rotate(traceType angle)
 }
 
 
-/// Get the angle of a vector, make sure its positive
+/**
+ * Get the angle of a vector, make sure its positive
+ * the domein of the atan is slighly different then we want it to be.
+ *  So given the quadrant of the vector, add a value to put it in our wanted domein
+ */
 traceType Vector2D::getAlpha() const {
-  /**
-   *  the domein of the atan is slighly different then we want it to be.
-   *  So given the quadrant of the vector, add a value to put it in our wanted domein
-   */    
+  if((x * x + y * y) < 0.001) {
+    std::cout<<"0 to 0";
+    return 0;
+  }
+
   traceType angle=atan2(y,x);
   if(angle<0)
     angle+=2*PI;
-
   return angle;
 }
 
-Vector2D& Vector2D::normalize(void)
-{
-	return (*this /= sqrt(x * x + y * y));
+Vector2D& Vector2D::normalize(void) {
+  return (*this /= sqrt(x * x + y * y));
 }
--- math/Vector2D.h
+++ math/Vector2D.h
@@ -6,56 +6,56 @@
 class Vector2D
 {
  public:
-  
+
   traceType   x;
   traceType  y;
-  
+
   Vector2D() {}
-        
+
   Vector2D(traceType r, traceType s)
     {
       x = r;
       y = s;
     }
- 
+
   Vector2D& Set(traceType r, traceType s)
     {
       x = r;
       y = s;
       return (*this);
     }
-  
+
   traceType& operator [](long k)
   {
     return ((&x)[k]);
   }
-  
+
   const traceType& operator [](long k) const
   {
     return ((&x)[k]);
   }
-  
+
   Vector2D& operator +=(const Vector2D& v)
     {
       x += v.x;
       y += v.y;
       return (*this);
     }
-  
+
   Vector2D& operator -=(const Vector2D& v)
     {
       x -= v.x;
       y -= v.y;
       return (*this);
     }
-  
+
   Vector2D& operator *=(traceType t)
     {
       x *= t;
       y *= t;
       return (*this);
     }
-  
+
   Vector2D& operator /=(traceType t)
     {
       traceType f = 1.0F / t;
@@ -63,29 +63,29 @@ class Vector2D
       y *= f;
       return (*this);
     }
-  
+
   Vector2D& operator &=(const Vector2D& v)
     {
       x *= v.x;
       y *= v.y;
       return (*this);
     }
-  
+
   Vector2D operator -(void) const
   {
     return (Vector2D(-x, -y));
   }
-  
+
   Vector2D operator +(const Vector2D& v) const
   {
     return (Vector2D(x + v.x, y + v.y));
   }
-  
+
   Vector2D operator -(const Vector2D& v) const
   {
     return (Vector2D(x - v.x, y - v.y));
   }
-  
+
   Vector2D operator /(traceType t) const
   {
     traceType f = 1.0F / t;
@@ -96,29 +96,29 @@ class Vector2D
     {
       return (Vector2D(x * t, y * t));
     }
-  
+
   traceType operator *(const Vector2D& v) const
   {
     return (x * v.x + y * v.y);
   }
-        
+
   Vector2D operator &(const Vector2D& v) const
   {
     return (Vector2D(x * v.x, y * v.y));
   }
-        
+
   bool operator ==(const Vector2D& v) const
   {
     return ((x == v.x) && (y == v.y));
   }
-        
+
   bool operator !=(const Vector2D& v) const
   {
     return ((x != v.x) || (y != v.y));
   }
-        
+
   Vector2D& normalize(void);
-      
+
   Vector2D& rotate(traceType angle);
   traceType getAlpha() const;
 };
