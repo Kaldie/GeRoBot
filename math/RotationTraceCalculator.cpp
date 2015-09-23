dae8f1bda54ab4bb813278bd1293a3ded2c4db33
kaldie <ruudcools@gmail.com>
2015-09-23 18:28:08 +0200

    Added functionality that ensures proper trace calculation

--- math/RotationTraceCalculator.cpp
+++ math/RotationTraceCalculator.cpp
@@ -15,66 +15,116 @@ RotationTraceCalculator::RotationTraceCalculator
     : LineTraceCalculator(i_robotPointer)
 {}
 
+
 RotationTraceCalculator::RotationTraceCalculator
 (Robot* i_robotPointer,
  const traceType& i_tolerance)
     : LineTraceCalculator(i_robotPointer, i_tolerance)
 {}
 
+
 void RotationTraceCalculator::calculateTrace
 (const RotationTrace& i_rotationTrace) {
-  if (i_rotationTrace.getTraceType() != Trace::Curve)
-    LOG_ERROR("Rotational trace calculator only works on rotational traces!"<<
-              "\nvalue: " << i_rotationTrace.getTraceType() <<
-              " curve value: " << Trace::Curve);
   std::vector<RotationTrace> traces = i_rotationTrace.getNecessaryTraces();
   LOG_DEBUG("Number of traces: " << traces.size());
-
   Point2D startPointOfThisTrace;
   for (std::vector<RotationTrace>::const_iterator itr = traces.begin();
        itr != traces.end();
        itr++) {
-    startPointOfThisTrace = m_robot->getVirtualPosition();
-
     LOG_DEBUG("Arc endPoint x, y: " << itr->getArc().getSecondPoint().x <<
               ", " << itr->getArc().getSecondPoint().y);
-
+    // This start position will be used to determine the current translation direction
+    startPointOfThisTrace = m_robot->getVirtualPosition();
+    // calculate the trace
     LineTraceCalculator::calculateTrace(*itr);
     // if there will be a next one
     if (std::next(itr) != traces.end()) {
-      auto nextTrace = std::next(itr);
-      // what is the current translation direction
-      std::string translationDirection =
-        itr->getTranslationDirectionToEndPoint(startPointOfThisTrace);
-      LOG_DEBUG("Current Translation direction " << translationDirection);
-
-      // what will be the current rotation direction
-      std::string rotationDirection =
-        nextTrace->getRotationDirectionToEndPoint(m_robot->getVirtualPosition());
-      LOG_DEBUG("Current Rotation direction " << rotationDirection);
-      /// Set one rotation step
-      if (getWriteLog())
-        writeToStepLog(rotationDirection, 1, m_robot->getVirtualPosition());
-      m_robot->prepareSteps(rotationDirection, 1);
-
-      // do set translation step untill the correction will be the same as the direcion we are stepping now
-      std::string currentCorrectionTranslationDirection;
-      Point2D intersectingPoint;
-      do {
-        LOG_DEBUG("Setting another translation step!");
-        // Prepare a set
-        m_robot->prepareSteps(translationDirection ,1);
-        if (getWriteLog())
-          writeToStepLog(translationDirection, 1, m_robot->getVirtualPosition());
-        LOG_INFO("Translating: " << translationDirection);
-        // get current intersecting point
-        intersectingPoint = nextTrace->intersectingPoint(m_robot->getVirtualPosition());
-        // determine the translation direction to that intersecting point
-        currentCorrectionTranslationDirection = nextTrace->getTranslationDirection
-          (m_robot->getVirtualPosition(), intersectingPoint);
-        LOG_DEBUG("Current correction direction :" << currentCorrectionTranslationDirection);
-        // if that is the same, stop
-      } while (currentCorrectionTranslationDirection != translationDirection);
+      // Additional steps will be set to ensure that the robot will follow the right path
+      ensureProperMovement(traces, itr, startPointOfThisTrace);
     }
   }
+}
+
+
+void RotationTraceCalculator::ensureProperMovement(const std::vector<RotationTrace>& i_traces,
+                                                   const std::vector<RotationTrace>::const_iterator& i_traceIterator,
+                                                   const Point2D& i_startPosition) {
+  auto nextTrace = std::next(i_traceIterator);
+  // what is the current translation direction
+  std::string translationDirection =
+     i_traceIterator->getTranslationDirectionToEndPoint(i_startPosition);
+  LOG_DEBUG("Current Translation direction " << translationDirection);
+  // Set a rotation step if appropate and let us know if we need to continue
+  if(!ensureRotationMovement(i_traces, nextTrace))
+    return;
+  // Now ensure translation steps are set to put it at an appropate position
+  ensureTranslationMovement(nextTrace, translationDirection);
+}
+
+
+bool RotationTraceCalculator::ensureRotationMovement(const std::vector<RotationTrace>& i_traces,
+                                                     const std::vector<RotationTrace>::const_iterator& i_trace) {
+   /// Determine if it is necessary to set a rotation step at all
+  if (!shouldRotateAtAll(i_traces, i_trace, m_robot->getVirtualPosition())) {
+    // if not, it is sure that we do not have to adjust the position here
+    return false;
+  }
+
+  /// Determine if it is necessary to set a rotation step
+  if (shouldRotate(*i_trace, m_robot->getVirtualPosition())) {
+    // what will be the current rotation direction
+    std::string rotationDirection =
+      i_trace->getRotationDirectionToEndPoint(m_robot->getVirtualPosition());
+    LOG_DEBUG("Current Rotation direction " << rotationDirection);
+    /// Set one rotation step
+    if (getWriteLog())
+      writeToStepLog(rotationDirection, 1, m_robot->getVirtualPosition());
+    m_robot->prepareSteps(rotationDirection, 1);
+  }
+  return true;
+}
+
+
+void RotationTraceCalculator::
+ensureTranslationMovement(const std::vector<RotationTrace>::const_iterator& i_trace,
+                          const std::string& i_requiredDirection) {
+  /*
+    do translation steps untill the correction will be
+    the same as the direcion we are stepping now
+  */
+  std::string currentCorrectionTranslationDirection;
+  Point2D intersectingPoint;
+  do {
+    LOG_DEBUG("Setting another translation step: " << i_requiredDirection << "!");
+    // Prepare a step
+    m_robot->prepareSteps(i_requiredDirection ,1);
+    if (getWriteLog())
+      writeToStepLog(i_requiredDirection, 1, m_robot->getVirtualPosition());
+    try {
+      // get current intersecting point
+      intersectingPoint = i_trace->intersectingPoint(m_robot->getVirtualPosition());
+    } catch (std::runtime_error) {
+      // if no intersecting point can be determined, we're done
+      break;
+    }
+    // determine the translation direction to that intersecting point
+    currentCorrectionTranslationDirection = i_trace->getTranslationDirection
+      (m_robot->getVirtualPosition(), intersectingPoint);
+    LOG_DEBUG("Current correction direction :" << currentCorrectionTranslationDirection);
+    // if that is the same, stop
+  } while (currentCorrectionTranslationDirection != i_requiredDirection);
+}
+
+
+bool RotationTraceCalculator::
+shouldRotateAtAll(const std::vector<RotationTrace>& i_traces,
+                  const std::vector<RotationTrace>::const_iterator& i_currentTrace,
+                  const Point2D& i_currentPosition) {
+  bool willRotate = false;
+  auto iterator = i_currentTrace;
+  while (iterator != i_traces.end() && !willRotate) {
+    willRotate |= shouldRotate(*iterator, m_robot->getVirtualPosition());
+    ++iterator;
+  }
+  return willRotate;
 }
--- math/RotationTraceCalculator.h
+++ math/RotationTraceCalculator.h
@@ -28,6 +28,33 @@ class RotationTraceCalculator: public LineTraceCalculator {
   RotationTraceCalculator
     (Robot* i_robot,
      const traceType& i_tolerance);
+
+  private:
+  /**
+   * Ensure the proper trace is calculated.
+   * An RotationTrace is calculated by splitting it up in several pieces.
+   * This will avoid sortcuts.
+   * However it is still possible that the incorrect trace is calculated
+   * When the robot is just before this the endpoint of this intermeditate trace
+   * the robot will walk back and will take a shortcut.
+   * This lead to very gross errors.
+   * By moving the robot before entering a new intermeditate trace this is effect is avoided
+   */
+  void ensureProperMovement(const std::vector<RotationTrace>& i_traces,
+                            const std::vector<RotationTrace>::const_iterator& i_traceIterator,
+                            const Point2D& i_startPosition);
+  /// Prepare rotation movement based on the actual position and necessity to move
+  bool ensureRotationMovement(const std::vector<RotationTrace>& i_traces,
+                              const std::vector<RotationTrace>::const_iterator& i_trace);
+  /// prepare translation movement to ensure the robot is behind the endpoint of the intermediate trace
+  void ensureTranslationMovement(const std::vector<RotationTrace>::const_iterator& i_trace,
+                                 const std::string& i_requiredDirection);
+  /// Returns if during this trace the robot needs to rotate
+  bool shouldRotateAtAll(const std::vector<RotationTrace>& i_traces,
+                    const std::vector<RotationTrace>::const_iterator& i_currentTrace,
+                    const Point2D& i_currentPosition);
+
+
 };
 
 #endif  // MATH_ROTATIONTRACECALCULATOR_H_
