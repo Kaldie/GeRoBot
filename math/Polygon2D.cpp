2b70f391b52e881e6c33558f76a3f7627280d708
kaldie <ruudcools@gmail.com>
2015-10-01 00:29:08 +0200

    A class which models a polygon

---
+++ math/Polygon2D.cpp
@@ -0,0 +1,142 @@
+/// Copyright Ruud Cools [2015]
+#include <macroHeader.h>
+#include "./Polygon2D.h"
+#include "./Point2D.h"
+
+/// easy constructor
+Polygon2D::Polygon2D() {}
+
+
+/// Constructor with vector
+Polygon2D::Polygon2D(const Point2DVector& i_vector)
+  : m_pointVector(i_vector) {
+}
+
+
+/// add an item to polygon
+void Polygon2D::addPoint2D(const Point2D& i_point) {
+  m_pointVector.push_back(i_point);
+}
+
+
+/// add an Point2D to the Polygon2D at the index
+void Polygon2D::addPoint2D(const Point2D& i_point, const size_t& i_index) {
+  if (m_pointVector.size() <= i_index) {
+    addPoint2D(i_point);
+  }
+  Point2DVector::iterator itr = m_pointVector.begin() + i_index;
+  m_pointVector.insert(itr, i_point);
+}
+
+
+/// returns the number of given points
+int Polygon2D::getNumberOfPoints() const {
+  return m_pointVector.size();
+}
+
+
+/// Determines if a point is within the Polygon2D
+bool Polygon2D::isInside(const Point2D i_point) const {
+  LOG_DEBUG("Point: " << i_point.x << " , " << i_point.y);
+  Line2D toOutSideLine = getOutsideLine(i_point);
+  LOG_DEBUG("Outside line: start " << toOutSideLine.getStartPoint().x << " , " << toOutSideLine.getStartPoint().y <<
+            " stop " << toOutSideLine.getEndPoint().x << " , " << toOutSideLine.getEndPoint().y);
+
+  int interSections = 0;
+  for (const auto& line : getLines()) {
+    if (line.intersects(toOutSideLine)) {
+      ++interSections;
+      LOG_DEBUG("Found intersection!");
+    }
+  }
+  if (interSections % 2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+/// Determines if the Polygon2D is closed
+bool Polygon2D::isClosed() const {
+  if(m_pointVector.front() == m_pointVector.back())
+    return true;
+  else
+    return false;
+}
+
+
+/// get the Line2Ds for all the points, closes the polygon if it is not closed
+std::vector<Line2D> Polygon2D::getLines() const {
+  Polygon2D newPoly(*this);
+  newPoly.closePolygon();
+  int numberOfLines = newPoly.getNumberOfPoints() - 2;
+  std::vector<Line2D> lines;
+  Point2DVector vector(newPoly.getPointVector());
+  for (int i = 0;
+       i != numberOfLines;
+       ++i) {
+    lines.push_back(Line2D(vector[i],
+			   vector[i+1]));
+  }
+  return lines;
+}
+
+
+/// If the polygon is not closed close it artificial by adding a extra point which is equal to the first
+bool Polygon2D::closePolygon() {
+  if (isClosed()) {
+    return false;
+  }
+  addPoint2D(m_pointVector.back());
+  return true;
+}
+
+
+/// Get a Points which lays outside the Polygon2D
+Line2D Polygon2D::getOutsideLine(Point2D i_point) const {
+  traceType min,max;
+  getXBounds(&min, &max);
+  Line2D minLine(i_point, Point2D(min - 10 * TOLERANCE, i_point.y));
+  Line2D maxLine(i_point, Point2D(max + 10 * TOLERANCE, i_point.y));
+  if (minLine.getLength() < maxLine.getLength()) {
+    return minLine;
+  } else {
+    return maxLine;
+  }
+}
+
+
+void Polygon2D::getXBounds(traceType* i_lowerBound,
+                           traceType* i_upperBound) const {
+  for (const auto& point : m_pointVector) {
+    if (*i_lowerBound > point.x) {
+      *i_lowerBound = point.x;
+    }
+    if (*i_upperBound < point.x) {
+      *i_upperBound = point.x;
+    }
+  }
+}
+
+
+bool Polygon2D::operator==(const Polygon2D& i_polygon) const {
+  // if the size is not equal, they are not equal
+  if (i_polygon.m_pointVector.size() !=
+      m_pointVector.size()) {
+    return false;
+  }
+  // do a point by point comparison
+  auto rhsIterator = i_polygon.m_pointVector.begin();
+  auto lhsIterator = m_pointVector.begin();
+  for (;
+       lhsIterator != m_pointVector.end();
+       ++lhsIterator, ++rhsIterator) {
+    // if one fails, they are not equal
+    if (*lhsIterator != *rhsIterator) {
+      return false;
+    }
+  }
+  // if all is equal, they are equal
+  return true;
+}
---
+++ math/Polygon2D.h
@@ -0,0 +1,43 @@
+//copyright [2015] Ruud Cools
+
+#ifndef MATH_POLYGON2D_H_
+#define MATH_POLYGON2D_H_
+
+#include "./Point2D.h"
+#include "./Line2D.h"
+
+class Polygon2D {
+  typedef std::vector<Point2D> Point2DVector;
+  GETSET(Point2DVector, m_pointVector, PointVector);
+ public:
+  /// easy constructor
+  Polygon2D();
+  /// Constructor with vector
+  Polygon2D(const Point2DVector&);
+  /// add an item to polygon
+  void addPoint2D(const Point2D& i_point);
+  /// add an Point2D to the Polygon2D at the index
+  void addPoint2D(const Point2D& i_point, const size_t& i_index);
+  /// returns the number of given points
+  int getNumberOfPoints() const;
+  /// Determines if a point is within the Polygon2D
+  bool isInside(const Point2D i_point) const;
+  /// Determines if the Polygon2D is closed
+  bool isClosed() const;
+  /// Determine if the polygon is empty
+  bool isEmpty() const {return m_pointVector.size() == 0;}
+  // is equal operator
+  bool operator==(const Polygon2D& i_polygon) const;
+  bool operator!=(const Polygon2D& i_polygon) const {return !operator==(i_polygon);}
+ private:
+  /// get the Line2Ds for all the points
+  std::vector<Line2D> getLines() const;
+  /// If the polygon is not closed close it artificial by adding a extra point which is equal to the first
+  bool closePolygon();
+  // returns the x bounds of the polygon
+  void getXBounds(traceType* i_lowerBound,
+                  traceType* i_upperBound) const;
+  // get the shortest line to a point outside the polygon
+  Line2D getOutsideLine(Point2D i_point) const;
+  };
+#endif  // MATH_POLYGON2D_H_
