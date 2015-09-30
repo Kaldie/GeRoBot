85d403cd2cfdafb8e97685ba6e88298f34326fec
kaldie <ruudcools@gmail.com>
2015-10-01 00:33:33 +0200

    Added object files for the new class dependancies and updated Line2DTest to check the new methods

--- math/unit_test/Line2DTest.h
+++ math/unit_test/Line2DTest.h
@@ -55,6 +55,42 @@ class Line2DTest : public CxxTest::TestSuite {
      Point2D intesection = line.getIntersectingPoint(Point2D(4, 40));
      TS_ASSERT_EQUALS(intesection, Point2D(1, 10));
   }
+
+  void testIntesects() {
+    Line2D line(startPoint,
+		endPoint);
+    Line2D line2(startPoint,
+		 endPoint);
+    // they run in parellel, so no intersection
+    TS_ASSERT(!line.intersects(line2));
+    Line2D line3(Point2D(0,4),
+		 Point2D(8,11));
+    // clean intersection
+    TS_ASSERT(line.intersects(line3));
+    Line2D line4(Point2D(0,4),
+		 Point2D(8,9));
+    // too short
+    TS_ASSERT(!line.intersects(line4));
+        Line2D line5(Point2D(10.5,4),
+		     Point2D(20,9));
+    // too far
+    TS_ASSERT(!line.intersects(line5));
+
+    Line2D line6(Point2D(0, 0 ),
+		 Point2D(0,11));
+    // intersects on point
+    TS_ASSERT(line.intersects(line6));
+
+    Line2D line8(Point2D(5, 0 ),
+		 Point2D(5,15));
+    // intersects on point
+    TS_ASSERT(line.intersects(line8));
+
+    Line2D line7(Point2D(0, 0 ),
+		 Point2D(0,10));
+    // intersects on point exectly
+    TS_ASSERT(line.intersects(line7));
+  }
 };
 
 #endif  // MATH_UNIT_TEST_LINE2DTEST_H_
