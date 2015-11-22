de9a8576410e0714eaf324f972591d9b575cf1e3
Ruud <ruudcools@gmail.com>
2015-11-22 22:16:35 +0100

    Yes, a real joint controller io unit test

--- motor/io/unit_test/JointControllerIOUnitTest.h
+++ motor/io/unit_test/JointControllerIOUnitTest.h
@@ -5,8 +5,11 @@
 
 #include <macroHeader.h>
 #include <cxxtest/TestSuite.h>
+#include <StepperDriver.h>
+#include <PinState.h>
 #include <JointControllerIO.h>
 #include <RobotIO.h>
+#include <BaseJoint.h>
 
 class JointControllerIOUnitTest : public CxxTest::TestSuite {
  public:
@@ -14,21 +17,108 @@ class JointControllerIOUnitTest : public CxxTest::TestSuite {
   void testBuild() {
     RobotIO robotIO("test_robot.xml");
     JointControllerIO jointControllerIO
-      (robotIO.getNodeFromPath(robotIO.getNodeFromPath("ROBOT"),"JOINTCONTROLLER"));
+      (robotIO.getNodeFromPath("ROBOT/JOINTCONTROLLER"));
     jointControllerIO.build();
     JointController jointController = jointControllerIO.getJointController();
+  }
+
+  void testBuildValues() {
+    RobotIO robotIO("test_robot.xml");
+    JointControllerIO jointControllerIO
+      (robotIO.getNodeFromPath("ROBOT/JOINTCONTROLLER"));
+    jointControllerIO.build();
+    JointController jointController = jointControllerIO.getJointController();
+    // test if the joint pointer vector is as expected
+    TS_ASSERT_EQUALS(jointController.getJointPointerVector().size(), 2);
     //Test if the rotation joint has a child, the translational joint!
     TS_ASSERT_EQUALS(jointController.resolveJoint(BaseJoint::Rotational)->getChild(),
                      jointController.resolveJoint(BaseJoint::Translational));
     // Test if the translational joint has no child
     TS_ASSERT_EQUALS(jointController.resolveJoint(BaseJoint::Translational)->getChild(),
                      nullptr);
+    TS_ASSERT_EQUALS(jointController.getSequenceVector().getSequenceVector().size(), 1);
+    // test if there is one state sequence is known
+    TS_ASSERT_EQUALS
+      (jointController.getSequenceVector().getSequenceVector()[0]
+       .getPinStateVector().size(),1);
+    // test if the usual pins are active
+    TS_ASSERT_EQUALS
+      (jointController.getSequenceVector().getSequenceVector()[0]
+       .getIntegerSequence(), std::vector<int>({252}));
   }
 
-  void testBuildValues() {
 
+  void testActuator() {
+    RobotIO robotIO("test_robot.xml");
+    JointControllerIO jointControllerIO
+      (robotIO.getNodeFromPath("ROBOT/JOINTCONTROLLER"));
+    jointControllerIO.build();
+    JointController controller = jointControllerIO.getJointController();
+    ArduinoMotorDriver driver =  controller.getActuator();
+    TS_ASSERT_EQUALS(driver.getSerialRegularExpresion(),"RuudWorld");
   }
 
+
+  void testEasyUpdate() {
+    // check if it is possible to update without inducing changes
+    RobotIO robotIO("test_robot.xml");
+    JointControllerIO jointControllerIO
+      (robotIO.getNodeFromPath(robotIO.getNodeFromPath("ROBOT"),"JOINTCONTROLLER"));
+    jointControllerIO.build();
+    JointController controller = jointControllerIO.getJointController();
+    jointControllerIO.update(controller);
+  }
+
+
+  void testUpdateDependancy() {
+    // check if it is possible to change the dependancy and
+    // see that reflected back into the document
+    RobotIO robotIO("test_robot.xml");
+    JointControllerIO jointControllerIO
+      (robotIO.getNodeFromPath("ROBOT/JOINTCONTROLLER"));
+    jointControllerIO.build();
+    JointController controller = jointControllerIO.getJointController();
+    controller.resolveJoint(BaseJoint::Translational)->setChild
+      (controller.resolveJoint(BaseJoint::Rotational));
+    jointControllerIO.update(controller);
+    robotIO.store("write.xml");
+    // loading the new xml document in a new RobotIO
+    RobotIO robotIO2("write.xml");
+    JointControllerIO jointControllerIO2
+      (robotIO2.getNodeFromPath("ROBOT/JOINTCONTROLLER"));
+    jointControllerIO2.build();
+    controller = jointControllerIO2.getJointController();
+    for (const auto& joint : controller.getJointPointerVector()) {
+      TS_ASSERT(joint->getChild());
+      if (joint->getChild() != controller.getJointPointerVector()[0] and
+	  joint->getChild() != controller.getJointPointerVector()[1])
+	LOG_ERROR("Should be able to find the child in the joint vector!");
+    }
+    //    std::remove("write.xml");
+  } 
+
+  void testRemoveAJoint() {
+    // check if it is possible to change the dependancy and
+    // see that reflected back into the document
+    RobotIO robotIO("test_robot.xml");
+    JointControllerIO jointControllerIO
+      (robotIO.getNodeFromPath("ROBOT/JOINTCONTROLLER"));
+    jointControllerIO.build();
+    JointController controller = jointControllerIO.getJointController();
+    JointController::JointPointerVector vector = controller.getJointPointerVector();
+    vector.pop_back();
+    controller.setJointPointerVector(vector);
+    jointControllerIO.update(controller);
+    robotIO.store("write2.xml");
+    // loading the new xml document in a new RobotIO
+    RobotIO robotIO2("write2.xml");
+    JointControllerIO jointControllerIO2
+      (robotIO2.getNodeFromPath("ROBOT/JOINTCONTROLLER"));
+    jointControllerIO2.build();
+    controller = jointControllerIO2.getJointController();
+    TS_ASSERT_EQUALS(controller.getJointPointerVector().size(), 1);
+    std::remove("write2.xml");
+  } 
 };
 
 
