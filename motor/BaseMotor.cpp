52d365d8d822f958efe9c574f024ff08b038121d
kaldie <ruudcools@gmail.com>
2015-11-30 18:39:55 +0100

    Import code style

--- motor/BaseMotor.cpp
+++ motor/BaseMotor.cpp
@@ -8,50 +8,53 @@
 
 // Constructors
 BaseMotor::BaseMotor()
-    : BaseMotor({1, 2, 3, 4},
-                "CW",
-                false)
+  : BaseMotor({1, 2, 3, 4},
+              "CW",
+              false)
 {}
 
+
 BaseMotor::BaseMotor(const PinVector& i_pins)
-    : BaseMotor(i_pins,
-                "CW",
-                false)
+  : BaseMotor(i_pins,
+              "CW",
+              false)
 {}
 
 
 BaseMotor::BaseMotor(const PinVector& i_pins,
                      const std::string& i_defaultDirection)
-    : BaseMotor(i_pins,
-                i_defaultDirection,
-                false)
+  : BaseMotor(i_pins,
+              i_defaultDirection,
+              false)
 {}
 
 
 BaseMotor::BaseMotor(const PinVector& i_pins,
                      const std::string& i_defaultDirection,
                      const bool i_holdMotor)
-    : m_defaultDirection(i_defaultDirection),
-      m_holdMotor(i_holdMotor),
-      m_speed(0) {
+  : m_defaultDirection(i_defaultDirection),
+    m_holdMotor(i_holdMotor),
+    m_speed(0) {
   setPins(i_pins);
 }
 
 
 void BaseMotor::setPins(const PinVector& i_pins) {
-    m_currentPinState.setPins(i_pins);
+  m_currentPinState.setPins(i_pins);
 }
+
 
 void BaseMotor::displayPinState() const {
-    displayPinState(m_currentPinState);
+  displayPinState(m_currentPinState);
 }
+
 
 void BaseMotor::displayPinState(const PinState& i_pinState) const {
-    i_pinState.displayPinState();
+  i_pinState.displayPinState();
 }
 
 
-void BaseMotor::displayPinStateSequence(
-    const StateSequence& i_stateSequence) const {
+void BaseMotor::displayPinStateSequence
+(const StateSequence& i_stateSequence) const {
   i_stateSequence.displaySequence();
 }
