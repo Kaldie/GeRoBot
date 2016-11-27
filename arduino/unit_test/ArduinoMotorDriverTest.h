1a8633ab140e6837b4474bda089faaf0fa58340e
Ruud <ruudcools@gmail.com>
2016-11-27 22:29:43 +0100

    we needed it

---
+++ arduino/unit_test/ArduinoMotorDriverTest.h
@@ -0,0 +1,30 @@
+// Copyright [2015] Ruud Cools
+
+#ifndef ARDUINO_UNITTEST_ARDUINOMOTORDRIVERTEST_H_
+#define ARDUINO_UNITTEST_ARDUINOMOTORDRIVERTEST_H_
+
+#include <macroHeader.h>
+#include <RobotIO.h>
+#include <ArduinoMotorDriver.h>
+#include "../ArduinoTool.h"
+
+
+class ArduinoMotorDriverTest : public CxxTest::TestSuite {
+public:
+   void testCreation() {
+     ArduinoMotorDriver driver = ArduinoMotorDriver();
+     ArduinoMotorDriver driver2 = ArduinoMotorDriver("Port");
+     TS_ASSERT_EQUALS(driver.getSerialRegularExpresion(), "");
+     TS_ASSERT_EQUALS(driver2.getSerialRegularExpresion(), "Port");
+   }
+
+   void testBenchmark() {
+           ArduinoMotorDriver driver = ArduinoMotorDriver("/dev/ttyUSB*");
+           driver.benchmarkSD(1000);
+   }
+
+};
+
+
+
+#endif  // ARDUINO_UNITTEST_ARDUINOMOTORDRIVERTEST_H_
