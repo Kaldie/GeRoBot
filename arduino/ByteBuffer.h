e5b7d53dc325fb4e14c4db4fbf066853d42b57fd
kaldie <ruudcools@gmail.com>
2016-12-23 20:49:38 +0100

    Changed the byte buffer so a malloc is no longer necessary. Much faster!!

--- arduino/ByteBuffer.h
+++ arduino/ByteBuffer.h
@@ -4,22 +4,20 @@
 /*
   Simple circular templated buffer
 */
-template <typename T>
+template <typename T, int N>
 class ByteBuffer {
  private:
   T* m_readPosition;
   T* m_writePosition;
-  T* m_buffer;
+  T m_buffer [N];
   T* m_endOfBuffer;
   int m_itemCount;
   void incrementPosition(T*& i_position);
-  ByteBuffer();
+
 
 
  public:
-  // Constructors
-  explicit ByteBuffer(const int& i_size);
-
+  ByteBuffer();
   // Deconstructor
   ~ByteBuffer();
   /// Get the element that is currently available to be read
@@ -52,51 +50,52 @@ class ByteBuffer {
   int inlineReadElements();
 };
 
+
 // Constructors
-template <typename T>
-ByteBuffer<T>::ByteBuffer(const int& i_size) {
-  m_buffer = (T*)malloc(sizeof(T) * i_size);
-  memset(m_buffer, 0, sizeof(T) * i_size);
-  m_endOfBuffer = m_buffer + i_size;
+template<typename T, int N>
+  ByteBuffer<T, N>::ByteBuffer() {
+  //  m_buffer = (T*)malloc(sizeof(T) * N);
+  memset(m_buffer, 0, sizeof(T) * N);
+  m_endOfBuffer = m_buffer + N;
   m_readPosition = m_buffer;
   m_writePosition = m_buffer;
   m_itemCount = 0;
 }
 
 // Deconstructor
-template <typename T>
-ByteBuffer<T>::~ByteBuffer() {
+template<typename T, int N>
+ByteBuffer<T, N>::~ByteBuffer() {
     free(m_buffer);
 }
 
-template <typename T>
-void ByteBuffer<T>::incrementPosition(T*& i_position) {
+template<typename T, int N>
+void ByteBuffer<T, N>::incrementPosition(T*& i_position) {
   i_position++;
   if (i_position >= m_endOfBuffer)
     i_position = m_buffer;
 }
 
 
-template <typename T>
-bool ByteBuffer<T>::isFull() {
+template<typename T, int N>
+bool ByteBuffer<T, N>::isFull() {
   return (m_endOfBuffer - m_buffer) <= m_itemCount;
 }
 
 
-template <typename T>
-bool ByteBuffer<T>::isEmpty() {
+template<typename T, int N>
+bool ByteBuffer<T, N>::isEmpty() {
   return m_itemCount == 0;
 }
 
 
-template <typename T>
-int ByteBuffer<T>::emptyElements() {
+template<typename T, int N>
+int ByteBuffer<T, N>::emptyElements() {
   return (m_endOfBuffer - m_buffer) - m_itemCount;
 }
 
 
-template <typename T>
-int ByteBuffer<T>::inlineWriteElements() {
+template<typename T, int N>
+int ByteBuffer<T, N>::inlineWriteElements() {
   if (isFull()) {
     return 0;
   }
@@ -108,8 +107,8 @@ int ByteBuffer<T>::inlineWriteElements() {
 }
 
 
-template <typename T>
-int ByteBuffer<T>::inlineReadElements() {
+template<typename T, int N>
+int ByteBuffer<T, N>::inlineReadElements() {
   if (isEmpty()) {
     return 0;
   }
@@ -121,14 +120,14 @@ int ByteBuffer<T>::inlineReadElements() {
 }
 
 
-template<typename T>
-T* ByteBuffer<T>::getReadPointer() {
+template<typename T, int N>
+T* ByteBuffer<T, N>::getReadPointer() {
   return m_readPosition;
 }
 
 
-template<typename T>
-bool ByteBuffer<T>::finishReadPointer() {
+template<typename T, int N>
+bool ByteBuffer<T, N>::finishReadPointer() {
   if (isEmpty()) {
     return false;
   } else {
@@ -139,8 +138,8 @@ bool ByteBuffer<T>::finishReadPointer() {
 }
 
 
-template<typename T>
-bool  ByteBuffer<T>::finishReadPointers(const int& i_finishes) {
+template<typename T, int N>
+bool  ByteBuffer<T, N>::finishReadPointers(const int& i_finishes) {
   bool result = true;
   for (int i = 0;
        i < i_finishes;
@@ -151,14 +150,14 @@ bool  ByteBuffer<T>::finishReadPointers(const int& i_finishes) {
 }
 
 
-template<typename T>
-T* ByteBuffer<T>::getWritePointer() {
+template<typename T, int N>
+T* ByteBuffer<T, N>::getWritePointer() {
   return m_writePosition;
 }
 
 
-template<typename T>
-bool ByteBuffer<T>::finishWritePointer() {
+template<typename T, int N>
+bool ByteBuffer<T, N>::finishWritePointer() {
   if (isFull()) {
     return false;
   } else {
@@ -169,8 +168,8 @@ bool ByteBuffer<T>::finishWritePointer() {
 }
 
 
-template<typename T>
-bool  ByteBuffer<T>::finishWritePointers(const int& i_finishes) {
+template<typename T, int N>
+bool  ByteBuffer<T, N>::finishWritePointers(const int& i_finishes) {
   bool result = true;
   for (int i = 0;
        i < i_finishes;
