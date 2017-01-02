52ad3553ac8b24f4e426fd2aa61db9a6359da839
kaldie <ruudcools@gmail.com>
2017-01-02 11:43:43 +0100

    Also writes down the direction

--- math/io/RotationTraceIO.cpp
+++ math/io/RotationTraceIO.cpp
@@ -1,22 +1,30 @@
 // Copyright [2015] Ruud Cools
 #include <macroHeader.h>
 #include <RotationTrace.h>
-#include "./RotationTraceIO.h"
-#include "./Point2DIO.h"
+#include <RotationTraceIO.h>
+#include <Point2DIO.h>
 #include <Arc2D.h>
+
 
 void RotationTraceIO::build() {
   LOG_DEBUG("Building a RotationTrace");
   TraceIO::build();
+  RotationTrace::RotationTracePointer pointer =
+    std::static_pointer_cast<RotationTrace>(m_tracePointer);
+  // Build centre point
   Point2DIO point2DIO(getNodeFromPath("./CenterPoint"));
   point2DIO.build();
-  RotationTrace::RotationTracePointer pointer =
-    std::dynamic_pointer_cast<RotationTrace>(getTracePointer());
-  if (!pointer) {
-    LOG_ERROR("Dynamic cast did not go well!");
-  }
   pointer->setCentrePoint(point2DIO.getPoint2D());
-  setTracePointer(pointer);
+  // set direction
+  std::string rotationDirection =
+    getNodeFromPath("./RotationDirection").text().as_string();
+  if (rotationDirection.compare("CCW") == 0) {
+    pointer->setIsClockwise(false);
+  } else if (rotationDirection.compare("CW") == 0) {
+    pointer->setIsClockwise(true);
+  } else {
+    LOG_ERROR("Unknown rotation direction");
+  }  
 }
 
 
@@ -29,13 +37,18 @@ RotationTraceIO::RotationTraceIO(const pugi::xml_node& i_node)
 bool RotationTraceIO::update(const Trace::TracePointer& i_trace) {
   LOG_DEBUG("Updateing Rotation Trace");
   bool hasUpdated = TraceIO::update(i_trace);
+  
   RotationTrace::RotationTracePointer pointer =
-    std::dynamic_pointer_cast<RotationTrace>(i_trace);
-  if (!pointer) {
-    LOG_ERROR("Dynamic cast went wrong. Which is very unexpected!");
-  }
-  //Point2D centerPoint = pointer->getArc().getCentrePoint();
+    std::static_pointer_cast<RotationTrace>(i_trace);
+
   Point2DIO point2DIO((getNodeFromPath("./CenterPoint")));
-  hasUpdated &= point2DIO.update(pointer->getArc().getCentrePoint());
+  hasUpdated &= point2DIO.update(pointer->getCentrePoint());
+
+  pugi::xml_node rotationDirectionNode = getNodeFromPath("./RotationDirection");
+  if (pointer->getIsClockwise()) {
+    rotationDirectionNode.text().set("CW");
+  } else {
+    rotationDirectionNode.text().set("CCW");
+  }  
   return hasUpdated;
 }
