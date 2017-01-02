cff7802c2c63ac09a362059514a2534247b047a6
kaldie <ruudcools@gmail.com>
2017-01-02 11:45:22 +0100

    added extreme point testing in unit test

--- math/unit_test/RotationTraceTest.h
+++ math/unit_test/RotationTraceTest.h
@@ -144,5 +144,45 @@ class RotationTraceTest : public CxxTest::TestSuite {
       TS_ASSERT_DELTA(RotationTrace(Point2D(13,5), Point2D(13, 5), Point2D(3, 5)).
                  getPerpendicularDistance(Point2D(3, 5)), 10, 0.0001);
   }
+
+  void testExtremePointsFullCirlce() {
+    LOG_DEBUG("testExtremePoints_FullCirlce");
+    RotationTrace trace(Point2D(10, 1), Point2D(10, 1), Point2D(0, 1));
+    std::vector<Point2D> pointVector;
+    trace.getExtremePoints(&pointVector);
+    TS_ASSERT_EQUALS(pointVector.size(),4);
+    TS_ASSERT_EQUALS(pointVector[0], Point2D(0, 11));
+    TS_ASSERT_EQUALS(pointVector[1], Point2D(-10, 1));
+    TS_ASSERT_EQUALS(pointVector[2], Point2D(0, -9));
+    TS_ASSERT_EQUALS(pointVector[3], Point2D(10, 1));
+  }
+
+  void testExtremePointsHalfCirlce() {
+    LOG_DEBUG("testExtremePoints_HalfCirlce");
+    RotationTrace trace(Point2D(10, 1), Point2D(-10, 1), Point2D(0, 1));
+    std::vector<Point2D> pointVector;
+    trace.getExtremePoints(&pointVector);
+    TS_ASSERT_EQUALS(pointVector.size(),1);
+    TS_ASSERT_EQUALS(pointVector[0], Point2D(0, 11));
+  }
+
+  void testExtremePointsHalfCirlceMinusABit() {
+    LOG_DEBUG("testExtremePointsHalfCirlceMinusABit");
+    RotationTrace trace(Point2D(10, 1), Point2D(-2.58819, 10.659258), Point2D(0, 1));
+    std::vector<Point2D> pointVector;
+    trace.getExtremePoints(&pointVector);
+    TS_ASSERT_EQUALS(pointVector.size(),1);
+    TS_ASSERT_EQUALS(pointVector[0], Point2D(0, 11));
+
+    trace.setIsClockwise(true);
+    pointVector.clear();
+    trace.getExtremePoints(&pointVector);
+    TS_ASSERT_EQUALS(pointVector.size(),3);
+    TS_ASSERT_EQUALS(pointVector[0], Point2D(10, 1));
+    TS_ASSERT_EQUALS(pointVector[1], Point2D(0, -9));
+    TS_ASSERT_EQUALS(pointVector[2], Point2D(-10, 1));
+  }
+
+  
 };
 #endif  // MATH_UNIT_TEST_ROTATIONTRACETEST_H_
