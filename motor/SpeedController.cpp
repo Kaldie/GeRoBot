91b6608bbe2e556a8b8088b834b5dc8b2a0b1f6b
kaldie <ruudcools@gmail.com>
2017-01-14 22:05:22 +0100

    adjusted the controllers to handle the movementregistrators

--- motor/SpeedController.cpp
+++ motor/SpeedController.cpp
@@ -4,34 +4,30 @@
 #include "SpeedController.h"
 #include <SequenceVector.h>
 #include <JointController.h>
-
+#include <MovementRegistrator.h>
 
 SpeedController::SpeedController()
-  : SpeedController(Type::None, 0, 0)
-{}
+  : SpeedController(Type::None, 0, 0) {
+}
 
 
 SpeedController::SpeedController(const Type& i_type)
-  : SpeedController(i_type, 0, 0){
+  : SpeedController(i_type, 0, 0) {
 }
 
+
 SpeedController::SpeedController(const Type& i_type, const float& i_robotSpeed)
-  : SpeedController(i_type, i_robotSpeed, 0){
+  : SpeedController(i_type, i_robotSpeed, 0) {
 }
+
 
 SpeedController::SpeedController(const Type& i_type,
 				 const float& i_robotSpeed,
 				 const int& i_currentSequenceVectorPosition)
   : m_type(i_type),
     m_robotSpeed(i_robotSpeed),
-    m_currentSequenceVectorPosition(i_currentSequenceVectorPosition)
-{
-}
-
-
-
-void SpeedController::notifyStep(const BaseJoint::JointPointer& i_joint,
-                                 const unsigned int& i_numberOfSteps) {
+    m_currentSequenceVectorPosition(i_currentSequenceVectorPosition),
+    m_movementRegistrator(std::make_shared<MovementRegistrator>()) {
 }
 
 
@@ -39,20 +35,16 @@ void SpeedController::acknowledgeSpeed(const unsigned int& i_speed,
                                        SequenceVector* i_sequenceVector) {
   LOG_DEBUG("Set motor speed is: " << i_speed);
   i_sequenceVector->modifySpeed(i_speed, m_currentSequenceVectorPosition);
-  m_currentSequenceVectorPosition = i_sequenceVector->getSequenceVector().size() - 1;
-
+  m_currentSequenceVectorPosition = i_sequenceVector->getSequenceVector().size();
 }
 
 
-void SpeedController::prepareSpeedController(const Trace& i_trace,
-                                            const JointController& i_controller) {
+std::weak_ptr<MovementRegistrator> SpeedController::getRegistrator() const {
+  return std::weak_ptr<MovementRegistrator>(m_movementRegistrator);
 }
 
 
-void SpeedController::prepareSpeedController(const BaseJoint::JointPointer& i_joint) {
-}
-
-
+/*
 SpeedController::JointStepVector SpeedController::estimateSteps
 (const Trace& i_trace, const JointController& i_controller) {
 
@@ -73,7 +65,8 @@ SpeedController::JointStepVector SpeedController::estimateSteps
   numberOfSteps = abs(magnitude(i_trace.getEndPoint()) - magnitude(i_trace.getStartPoint()) /
         currentJoint->getMovementPerStep());
 
-  jointStepVector.push_back
-    (std::make_tuple(currentJoint, numberOfSteps));
+  jointStepVector.push_back(std::make_tuple(currentJoint, numberOfSteps));
   return jointStepVector;  
 }
+*/
+
--- motor/SpeedController.h
+++ motor/SpeedController.h
@@ -8,22 +8,27 @@ class Trace;
 class JointController;
 class SequenceVector;
 class BaseJoint;
+class MovementRegistrator;
 // Interface of speed controllers, there will be some
 class SpeedController {
 public:
-  enum Type {None, Constant, Prescribed, Relative};
+  enum Type {None, Constant, Prescribed, Relative, Manual};
 
   typedef  std::shared_ptr<SpeedController> SpeedControllerPointer;
   /// Defines the type of speed controller
   GETSET(Type, m_type, Type);
+
   /// Defines the requested speed of the robot in mm/sec
   GETSETPROTECTED(float, m_robotSpeed, RobotSpeed);
+
   /// Defines the current position the speedcontroller will update the sequence vector to achieve the selected speed
   GETSET(int, m_currentSequenceVectorPosition, CurrentSequenceVectorPosition);
 
-  /// The controller is notified that steps will be taken
-  virtual void notifyStep(const std::shared_ptr<BaseJoint>& i_joint,
-                          const unsigned int& i_numberOfSteps);
+  /// The registrator that will be called when the robot will move
+  GETSETPROTECTED(std::shared_ptr<MovementRegistrator>, m_movementRegistrator, MovementRegistrator);
+
+  /// get the movement registrator
+  virtual std::weak_ptr<MovementRegistrator> getRegistrator() const;
 
   /// This method will be called at the moment the robot needs to decied on some speed
   virtual bool adviseSpeed(int* o_speed) const = 0;
@@ -33,23 +38,7 @@ public:
    */
   virtual void acknowledgeSpeed(const unsigned int& i_speed,
                                 SequenceVector* i_sequenceVector);
-
-  /**
-
-   * This is call just before a algorithm determins movement of the robot
-   *
-   */
-  virtual void prepareSpeedController(const Trace& i_trace, const JointController&);
-
-  /**
-   * Prepare the controller that steps with this joint will be set
-   * it is unknown how many and in which direction
-   * if a "Constant" type controller is used, use the current position as a base to calculate the speed
-   */
-  virtual void prepareSpeedController(const std::shared_ptr<BaseJoint>& i_pointer);
-
-  SpeedController();
-
+  
   SpeedController(const Type& i_type);
   
   SpeedController(const Type& i_type,
@@ -58,12 +47,9 @@ public:
   SpeedController(const Type& i_type,
 		  const float& i_robotSpeed,
 		  const int& i_currentSequenceVectorPosition);
- protected:  
-  typedef std::vector<std::tuple<std::shared_ptr<BaseJoint>,int>> JointStepVector;
-  /**
-   * Brief: estimate the number of steps for each joint has to set during to complete the trace
-   */
-  JointStepVector estimateSteps(const Trace& i_trace, const JointController& i_controller);
+
+ private:
+  SpeedController();
 };
 
 #endif  // MOTOR_SPEEDCONTROLLER_H_
